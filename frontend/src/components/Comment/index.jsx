82cab70a83b9716d3a4b1f576a4dbdfd0e69a0a6
Jonathan <Jonnaa96@yahoo.com>
2023-11-06 17:38:31 -0700

    Comment component basic setup

--- frontend/src/components/Comment/index.jsx
+++ frontend/src/components/Comment/index.jsx
@@ -1,5 +1,70 @@
-export default function Comment({data}){
+import { useState } from "react"
+import { updateComment, deleteComment } from "../../../utils/backend"
+
+export default function Comment({data, refreshComments}){
+    const [editMode, setEditMode] = useState(false)
+    const [editModeData, setEditModeData]= useState({
+        title: data.title,
+        img: data.img,
+        description: data.description
+    })
+
+    function handleInputChange(event) {
+        setEditModeData({
+            ...editModeData,
+            [event.target.name]: event.target.value
+        })
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        setEditMode(false)
+        updateComment(editModeData, data._id)
+            .then(() => refreshComments())
+    }
+
+    function handleDelete() {
+        deleteComment(data._id)
+            .then(() => refreshComments())
+    }
+
+    // Edit mode false
+    let commentDisplay =<div>
+            <p>{data.title}</p>
+            <img src={data.img} alt="" />
+            <p>{data.description}</p>
+            <button onClick={()=>{setEditMode(true)}}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+
+    // Edit mode true
+    if(editMode){
+        commentDisplay = <form onSubmit={handleSubmit}>
+            <input 
+                name="title"
+                value={editModeData.title}
+                onChange={handleInputChange}
+                required
+            />
+            <br />
+            <input 
+                name="img"
+                value={editModeData.img}
+                onChange={handleInputChange}
+                required
+            />
+            <br />
+            <input 
+                name="description"
+                value={editModeData.description}
+                onChange={handleInputChange}
+                required
+            />
+            <button onClick={()=>{setEditMode(false)}}>Cancel</button>
+            <button type="submit">Submit</button>
+        </form>
+    }
+
     return(
-        <h1>comment</h1>
+        {commentDisplay}
     )
 }
--- frontend/src/components/CommentsGallery/index.jsx
+++ frontend/src/components/CommentsGallery/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getComments, createComment } from "../../../utils/backend"
+import Comment from "../Comment"
 
 export default function CommentsGallery({id}){
     const [comments, setComments] = useState([])
@@ -9,13 +10,20 @@ export default function CommentsGallery({id}){
             .then(comments => setComments(comments))
     }, [])
 
+    function refreshComments() {
+        getComments(id)
+            .then(newCommentData => setComments(newCommentData))
+    }
+
     let commentGallery = <p>No one has commented on this post...</p>
 
     if(comments>0){
         commentGallery = comments.map(comment=>{
-            return <p></p>
+            return <Comment key={comment._id} data={comment} refreshComments={refreshComments}/>
         })
     }
+
+
     return(
         <div>
             {commentGallery}
